2db1100297171c13098ccd77b04bcd7524de4c6c
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2020-10-12 14:01:23 -0400

    updated package.json to realm@rc2 and added auth in renderer.js

--- renderer.js
+++ renderer.js
@@ -1,1 +1,14 @@
 const Realm = require("realm");
+
+console.log("Realm", Realm);
+
+async function run() {
+  const realmApp = new Realm.App({ id: "tutsbrawl-qfxxj" }); // Replace <Your App ID> with your application id
+  let credentials = Realm.Credentials.anonymous();
+  // log in anonymously
+  let user = await realmApp.logIn(credentials);
+
+  console.log("user.id", user.id);
+}
+
+run();
