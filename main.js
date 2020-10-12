d23d1755e51bfd83f298b6d2678f06c2a13c7bce
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2020-10-12 13:51:13 -0400

    basic electron app

---
+++ main.js
@@ -0,0 +1,23 @@
+const electron = require("electron");
+
+const Realm = require("realm");
+
+const { app, BrowserWindow } = electron;
+
+function createWindow() {
+  // Create the browser window.
+
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  console.log("main: Realm", Realm);
+  // and load the index.html of the app.
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
