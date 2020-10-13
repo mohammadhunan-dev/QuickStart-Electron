ceea2ac3567c105b53e438599d46c8c1605f5b4a
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2020-10-12 22:40:32 -0400

    added realm writes

--- main.js
+++ main.js
@@ -1,23 +1,67 @@
-const electron = require("electron");
-
+const { app, BrowserWindow, ipcMain } = require("electron");
 const Realm = require("realm");
 
-const { app, BrowserWindow } = electron;
-
 function createWindow() {
-  // Create the browser window.
-
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+  const mainWindow = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
-  console.log("main: Realm", Realm);
-  // and load the index.html of the app.
-  win.loadFile("index.html");
+  mainWindow.loadFile("index.html");
+  mainWindow.webContents.openDevTools();
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(async () => {
+  const realmApp = new Realm.App({ id: "tutsbrawl-qfxxj" }); // Replace <Your App ID> with your application id
+  let credentials = Realm.Credentials.anonymous();
+  // log in anonymously
+  let user = await realmApp.logIn(credentials);
+
+  const PersonSchema = {
+    name: "Person",
+    properties: {
+      _id: "objectId",
+      name: "string",
+    },
+    primaryKey: "_id",
+  };
+
+  const config = {
+    schema: [PersonSchema],
+    path: "myrealm.realm",
+    sync: {
+      user: user,
+      partitionValue: "My Partition",
+    },
+  };
+  // open a synced realm
+  const realm = await Realm.open(config);
+
+  // Get all Persons in the realm
+  const persons = realm.objects("Person");
+
+  // when receiving an "asynchronous-message" from the renderer process,
+  // upload all local changes to the synced realm
+  ipcMain.on("asynchronous-message", (event, arg) => {
+    if (arg === "sync") {
+      realm.syncSession.uploadAllLocalChanges();
+      console.log("main process: Syncing all local changes");
+      console.log(
+        `main process: Created person object: \t ${JSON.stringify(
+          persons[0],
+          null,
+          2
+        )}`
+      );
+
+      // send a reply to the renderer process with the created person's name
+      event.reply(
+        "asynchronous-reply",
+        `created person object with the name ${persons[0].name}`
+      );
+    }
+  });
+
+  createWindow();
+});
--- renderer.js
+++ renderer.js
@@ -1,6 +1,6 @@
+const { ipcRenderer } = require("electron");
 const Realm = require("realm");
-
-console.log("Realm", Realm);
+const ObjectId = require("bson").ObjectId;
 
 async function run() {
   const realmApp = new Realm.App({ id: "tutsbrawl-qfxxj" }); // Replace <Your App ID> with your application id
@@ -8,7 +8,41 @@ async function run() {
   // log in anonymously
   let user = await realmApp.logIn(credentials);
 
-  console.log("user.id", user.id);
+  var PersonSchema = {
+    name: "Person",
+    properties: {
+      _id: "objectId",
+      name: "string",
+    },
+    primaryKey: "_id",
+  };
+
+  const config = {
+    path: "myrealm.realm",
+    schema: [PersonSchema],
+    sync: true,
+  };
+
+  // open a non synced realm
+
+  const realm = new Realm(config);
+
+  const personList = realm.objects("Person");
+
+  // create a new "Person"
+  realm.write(() => {
+    john = realm.create("Person", {
+      _id: new ObjectId(),
+      name: "John Smith",
+    });
+  });
+
+  ipcRenderer.send("asynchronous-message", "sync");
+
+  // receive a reply with the created person's name from the main process
+  ipcRenderer.on("asynchronous-reply", (event, arg) => {
+    console.log(`renderer process:`, arg); // prints "pong"
+  });
 }
 
 run();
